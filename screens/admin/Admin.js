27fc2d430fb9084c380ff2cce619ceafd76b19ae
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2021-01-17 16:47:16 +0100

    Flatlist añadida en Admin

--- screens/admin/Admin.js
+++ screens/admin/Admin.js
@@ -11,22 +11,57 @@ import {
   StyleSheet,
   View,
   ScrollView,
+  FlatList
 } from 'react-native';
 import { Bet } from '../../components/bet/Bet';
 import { Find } from '../../components/find/Find';
 
+const DATA = [
+  {
+    id: '1',
+  },
+  {
+    id: '2',
+  },
+  {
+    id: '3',
+  },
+  {
+    id: '4',
+  },
+  {
+    id: '5',
+  },
+  {
+    id: '6',
+  },
+  {
+    id: '7',
+  },
+  {
+    id: '8',
+  },
+  {
+    id: '9',
+  },
+  {
+    id: '10',
+  },
+];
+
 class Admin extends Component {
   render() {
+    const renderItem = () => (
+      <Bet></Bet>
+    );
     return (
       <View style={styles.contenidor}>
         <ScrollView>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
+          <FlatList
+            data={DATA}
+            keyExtractor={item => item.id}
+            renderItem={renderItem}
+          />
         </ScrollView>
         <Find></Find>
       </View>
