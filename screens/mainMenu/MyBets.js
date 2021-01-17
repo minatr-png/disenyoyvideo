a9e1db307d2bbfe18f62128e25618851d0a7ebc5
Miguel Navarro Travé <otracuentaderrellenomas@gmail.com>
2021-01-17 16:45:16 +0100

    Flatlist añadida en MyBets

--- screens/mainMenu/MyBets.js
+++ screens/mainMenu/MyBets.js
@@ -11,22 +11,48 @@ import {
   StyleSheet,
   View,
   ScrollView,
+  FlatList
 } from 'react-native';
 import { Navegacio } from '../../components/navegacio/Navegacio';
 import { Bet } from '../../components/bet/Bet';
 
+const DATA = [
+  {
+    id: '1',
+  },
+  {
+    id: '2',
+  },
+  {
+    id: '3',
+  },
+  {
+    id: '4',
+  },
+  {
+    id: '5',
+  },
+  {
+    id: '6',
+  },
+  {
+    id: '7',
+  },
+];
+
 class MyBets extends Component {
   render() {
+    const renderItem = () => (
+      <Bet></Bet>
+    );
     return (
       <View style={styles.contenidor}>
         <ScrollView>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
-          <Bet></Bet>
+          <FlatList
+            data={DATA}
+            keyExtractor={item => item.id}
+            renderItem={renderItem}
+          />
         </ScrollView>
         <Navegacio></Navegacio>
       </View>
